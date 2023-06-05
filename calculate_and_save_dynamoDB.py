901224b14e74ea19018fe222c9383df28d27f17b
Chirag Kular <kular_chirag@yahoo.com>
2023-06-04 22:53:10 -0500

    Create calculate_and_save_dynamoDB.py
    Created an Amplify Web app, API Gateway REST API, Lambda Function with inline policy to save data to dynamoDB

---
+++ calculate_and_save_dynamoDB.py
@@ -0,0 +1,23 @@
+import json
+import math
+import boto3
+from time import gmtime, strftime
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('PowerOfMathDatabase')
+
+now = strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())
+
+def lambda_handler(event, context):
+    # TODO implement
+    mathResult = math.pow(int(event['base']), int(event['exponent']))
+    
+    response = table.put_item(
+        Item= {
+            'ID': str(mathResult),
+            'LatestGreetingTime': now
+        })
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Your result!' + str(mathResult))
+    }
