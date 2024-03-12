c13d8255d8d324ab6153d44767795935b77fccca
Chirag Kular <kular_chirag@yahoo.com>
2024-03-12 13:35:11 +0000

    instructions

--- CDK-V2/cdk-starter/lib/cdk-starter-stack.ts
+++ CDK-V2/cdk-starter/lib/cdk-starter-stack.ts
@@ -37,15 +37,26 @@ export class CdkStarterStack extends cdk.Stack {
       }
     })
 
+    const duration = new cdk.CfnParameter(this, 'duration', {
+      default: 6,
+      minValue: 1,
+      maxValue: 10,
+      type: 'Number'
+    })
+
     // L2 Construct
-    new Bucket(this, 'MyL2Bucket', {
+    const myL2Bucket = new Bucket(this, 'MyL2Bucket', {
       lifecycleRules: [{
-        expiration: cdk.Duration.days(2)
+        expiration: cdk.Duration.days(duration.valueAsNumber)
       }]
     })
 
     // L3 Construct
     new L3Bucket(this, 'MyL3Bucket', 3);
 
+    // Cfn Outputs - shown after deployment
+    new cdk.CfnOutput(this, 'MyL2BucketName', {
+      value: myL2Bucket.bucketName
+    })
   }
 }
